27b1250823e40c4c6e65de7036605bd28a000757
DanielBerrio272 <danielberriog@hotmail.com>
2025-05-12 18:51:21 -0500

    Update trabajo.js

--- trabajo.js
+++ trabajo.js
@@ -2,12 +2,11 @@ function comida() {
     const imagen = document.getElementById('dibujo');
     const musica = document.getElementById('musica');
 
-    // Mostrar imagen con transición
+
     imagen.classList.remove('oculto');
     setTimeout(() => {
         imagen.style.opacity = 1;
-    }, 100); // retrasa para que la transición se aplique
+    }, 100);
 
-    // Reproducir música
     musica.play();
 }
